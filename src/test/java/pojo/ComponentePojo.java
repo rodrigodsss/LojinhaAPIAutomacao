f50d6df26e25d7c6b98568a4da321d6c26d2bdab
Rodrigo Sousa <rodrigodss89@gmail.com>
2023-05-08 21:12:43 -0300

    Inicializando o repositório com os arquivos iniciais do projeto

---
+++ src/test/java/pojo/ComponentePojo.java
@@ -0,0 +1,25 @@
+package pojo;
+
+public class ComponentePojo {
+    private String componenteNome;
+    private int componenteQuantidade;
+
+    public String getComponenteNome() {
+        return componenteNome;
+    }
+
+    public void setComponenteNome(String componenteNome) {
+        this.componenteNome = componenteNome;
+    }
+
+    public int getComponenteQuantidade() {
+        return componenteQuantidade;
+    }
+
+    public void setComponenteQuantidade(int componenteQuantidade) {
+        this.componenteQuantidade = componenteQuantidade;
+    }
+
+    public void add(ComponentePojo componente) {
+    }
+}
